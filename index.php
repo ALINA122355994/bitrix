739c29dddf7dac42b67183ecd28f0d3b46efcdde
Alina <gaffarova_2001@mail.ru>
2024-11-27 00:29:08 +0300

    task #177651

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ $APPLICATION->SetTitle("Мебельная компания");
 
 <div class="slide-one-item home-slider owl-carousel">
 
-<div class="site-blocks-cover" style="background-image: url(images/hero_bg_1.jpg);" data-aos="fade"
+<div class="site-blocks-cover" style="background-image: url(local/templates/home/images/hero_bg_1.jpg);" data-aos="fade"
   data-stellar-background-ratio="0.5">
 
   <div class="text">
@@ -110,7 +110,7 @@ $APPLICATION->SetTitle("Мебельная компания");
   </div>
 </div>
 
-<div class="site-blocks-cover" style="background-image: url(images/hero_bg_3.jpg);" data-aos="fade"
+<div class="site-blocks-cover" style="background-image: url(local/templates/home/images/hero_bg_3.jpg);" data-aos="fade"
   data-stellar-background-ratio="0.5">
 
   <div class="text">
